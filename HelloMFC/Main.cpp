f93c2bac6ec60e6a52d48823303472075f6a7aac
illidan <smile@illidan.org>
2017-10-16 11:48:28 +0800

    MFC - 使用消息映射（自定义函数）

--- HelloMFC/Main.cpp
+++ HelloMFC/Main.cpp
@@ -11,63 +11,61 @@
 
 class MyFrameWnd : public CFrameWnd
 {
-	//DECLARE_MESSAGE_MAP()
-protected:
-	static const AFX_MSGMAP* PASCAL GetThisMessageMap();
-	virtual const AFX_MSGMAP* GetMessageMap() const;
-
+	DECLARE_MESSAGE_MAP()
 public:
-	afx_msg int OnCreate(LPCREATESTRUCT);
-	afx_msg void OnPaint();
-	afx_msg void OnSize(UINT, int, int);
+	//afx_msg int OnCreate(LPCREATESTRUCT);
+	//afx_msg void OnPaint();
+	//afx_msg void OnSize(UINT, int, int);
+	afx_msg LRESULT MyCreate(WPARAM wParam, LPARAM lParam);
+	afx_msg LRESULT MyPaint(WPARAM wParam, LPARAM lParam);
+	afx_msg LRESULT MySize(WPARAM wParam, LPARAM lParam);
 };
 
-//BEGIN_MESSAGE_MAP(MyFrameWnd, CFrameWnd)
-//	ON_WM_CREATE()
-//	ON_WM_PAINT()
-//	ON_WM_SIZE()
-//END_MESSAGE_MAP()
+BEGIN_MESSAGE_MAP(MyFrameWnd, CFrameWnd)
+	//ON_WM_CREATE()
+	//ON_WM_PAINT()
+	//ON_WM_SIZE()
+	ON_MESSAGE(WM_CREATE, MyCreate)
+	ON_MESSAGE(WM_PAINT, MyPaint)
+	ON_MESSAGE(WM_SIZE, MySize)
+END_MESSAGE_MAP()
 
-//PTM_WARNING_DISABLE
-__pragma(warning(push))
-__pragma(warning(disable : 4867))
-const AFX_MSGMAP* MyFrameWnd::GetMessageMap() const
-{ 
-	return GetThisMessageMap(); 
-}
-const AFX_MSGMAP* PASCAL MyFrameWnd::GetThisMessageMap()
-{
-	typedef MyFrameWnd ThisClass;
-	typedef CFrameWnd TheBaseClass;
-	static const AFX_MSGMAP_ENTRY _messageEntries[] =
-	{
-		{ WM_CREATE, 0, 0, 0, AfxSig_is, (AFX_PMSG)(AFX_PMSGW)(static_cast<int (AFX_MSG_CALL CWnd::*)(LPCREATESTRUCT)> (&ThisClass::OnCreate)) },
-		{ WM_PAINT, 0, 0, 0, AfxSig_vv, (AFX_PMSG)(AFX_PMSGW)(static_cast<void (AFX_MSG_CALL CWnd::*)(void)> (&ThisClass::OnPaint)) },
-		{ WM_SIZE, 0, 0, 0, AfxSig_vwii, (AFX_PMSG)(AFX_PMSGW)(static_cast<void (AFX_MSG_CALL CWnd::*)(UINT, int, int)> (&ThisClass::OnSize)) },
-		{ 0, 0, 0, 0, AfxSig_end, (AFX_PMSG)0 }
-	};
-	static const AFX_MSGMAP messageMap = { &TheBaseClass::GetThisMessageMap, &_messageEntries[0] };
-	return &messageMap;
-}
-__pragma(warning(pop))
-//PTM_WARNING_RESTORE
+//int MyFrameWnd::OnCreate(LPCREATESTRUCT)
+//{
+//	AfxMessageBox(TEXT("On Create!"));
+//	return 0;
+//}
+//void MyFrameWnd::OnPaint()
+//{
+//	CDC* cdc;
+//	PAINTSTRUCT pt;
+//	cdc = BeginPaint(&pt);
+//	cdc->TextOut(0, 0, TEXT("Hello Text!"));
+//	EndPaint(&pt);
+//}
+//void MyFrameWnd::OnSize(UINT, int, int)
+//{
+//	AfxMessageBox(TEXT("On Size!"));
+//}
 
-int MyFrameWnd::OnCreate(LPCREATESTRUCT)
+afx_msg LRESULT MyFrameWnd::MyCreate(WPARAM wParam, LPARAM lParam)
 {
 	AfxMessageBox(TEXT("On Create!"));
 	return 0;
 }
-void MyFrameWnd::OnPaint()
+afx_msg LRESULT MyFrameWnd::MyPaint(WPARAM wParam, LPARAM lParam)
 {
 	CDC* cdc;
 	PAINTSTRUCT pt;
 	cdc = BeginPaint(&pt);
 	cdc->TextOut(0, 0, TEXT("Hello Text!"));
 	EndPaint(&pt);
+	return 0;
 }
-void MyFrameWnd::OnSize(UINT, int, int)
+afx_msg LRESULT MyFrameWnd::MySize(WPARAM wParam, LPARAM lParam)
 {
 	AfxMessageBox(TEXT("On Size!"));
+	return 0;
 }
 
 class MyApp : public CWinApp
