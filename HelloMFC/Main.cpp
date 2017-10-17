1f04b91ce5ee39ad01ab498246becddf39cc8743
illidan <smile@illidan.org>
2017-10-17 10:15:32 +0800

    MFC - 工具栏

--- HelloMFC/Main.cpp
+++ HelloMFC/Main.cpp
@@ -8,6 +8,7 @@
  *
  ****************************************************************************************************************/
 #include <afxwin.h>
+#include <afxext.h>
 #include "resource.h"
 
 class MyFrameWnd : public CFrameWnd
@@ -20,6 +21,7 @@ public:
 	afx_msg void Exit();
 private:
 	CMenu m_Menu;
+	CToolBar m_ToolBar;
 };
 
 BEGIN_MESSAGE_MAP(MyFrameWnd, CFrameWnd)
@@ -27,12 +29,25 @@ BEGIN_MESSAGE_MAP(MyFrameWnd, CFrameWnd)
 	ON_COMMAND(ID_NEW, New)
 	ON_COMMAND(ID_SAVE, Save)
 	ON_COMMAND(ID_EXIT, Exit)
+	ON_COMMAND(ID_A, New)
+	ON_COMMAND(ID_B, Save)
+	ON_COMMAND(ID_C, Exit)
 END_MESSAGE_MAP()
 
 afx_msg LRESULT MyFrameWnd::Create(WPARAM wParam, LPARAM lParam)
 {
+	//菜单
 	m_Menu.LoadMenu(IDR_MENU1);
 	SetMenu(&m_Menu);
+
+	//工具条
+	m_ToolBar.Create(this);
+	m_ToolBar.SetWindowText(TEXT("工具条"));
+	m_ToolBar.LoadToolBar(IDR_TOOLBAR1);
+	m_ToolBar.EnableDocking(CBRS_ALIGN_ANY);
+	EnableDocking(CBRS_ALIGN_ANY);
+	DockControlBar(&m_ToolBar, AFX_IDW_DOCKBAR_LEFT);
+
 	return 0;
 }
 
