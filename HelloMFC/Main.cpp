40e0d1e835b96299301ffdad1cbd1faf78d11bc8
illidan <smile@illidan.org>
2017-10-26 16:39:39 +0800

    MFC - CCreateContext
    使用CCreateContext创建视图类。

--- HelloMFC/Main.cpp
+++ HelloMFC/Main.cpp
@@ -8,12 +8,15 @@
  *
  ****************************************************************************************************************/
 #include <afxwin.h>
+#include <afxext.h>
 
 class MyView : public CView
 {
+	DECLARE_DYNCREATE(MyView)
 public:
 	virtual void OnDraw(CDC* pDC);
 };
+IMPLEMENT_DYNCREATE(MyView, CView)
 
 void MyView::OnDraw(CDC* pDC)
 {
@@ -24,26 +27,26 @@ class MyFrameWnd : public CFrameWnd
 {
 	DECLARE_MESSAGE_MAP()
 public:
-	afx_msg int OnCreate(LPCREATESTRUCT);
+	//afx_msg int OnCreate(LPCREATESTRUCT);
 	afx_msg void OnPaint();
 
-private:
-	MyView* pView;
+//private:
+//	MyView* pView;
 };
 
 BEGIN_MESSAGE_MAP(MyFrameWnd, CFrameWnd)
-	ON_WM_CREATE()
+	//ON_WM_CREATE()
 	ON_WM_PAINT()
 END_MESSAGE_MAP()
 
-afx_msg int MyFrameWnd::OnCreate(LPCREATESTRUCT lpCreateStruct)
-{
-	pView = new MyView;
-	pView->Create(NULL, TEXT("View"), WS_CHILD | WS_VISIBLE | WS_BORDER, CRect(200, 200, 600, 600), this, 0);
-	SetActiveView(pView);
-
-	return CFrameWnd::OnCreate(lpCreateStruct);
-}
+//afx_msg int MyFrameWnd::OnCreate(LPCREATESTRUCT lpCreateStruct)
+//{
+//	pView = new MyView;
+//	pView->Create(NULL, TEXT("View"), WS_CHILD | WS_VISIBLE | WS_BORDER, CRect(200, 200, 600, 600), this, 0);
+//	SetActiveView(pView);
+//
+//	return CFrameWnd::OnCreate(lpCreateStruct);
+//}
 
 afx_msg void MyFrameWnd::OnPaint()
 {
@@ -67,8 +70,16 @@ END_MESSAGE_MAP()
 
 BOOL MyApp::InitInstance()
 {
+	CCreateContext* pContext = new CCreateContext;
+	pContext->m_pCurrentDoc = NULL;
+	pContext->m_pCurrentFrame = NULL;
+	pContext->m_pLastView = NULL;
+	pContext->m_pNewDocTemplate = NULL;
+	pContext->m_pNewViewClass = RUNTIME_CLASS(MyView);
+
 	CFrameWnd* pFrameWnd = new MyFrameWnd;
-	pFrameWnd->Create(NULL, TEXT("illidan"));
+	//使用pContext动态创建MyView。必须在类MyView中使用宏：DECLARE_DYNCREATE(MyView) 和 IMPLEMENT_DYNCREATE(MyView, CView)
+	pFrameWnd->Create(NULL, TEXT("illidan"), WS_OVERLAPPEDWINDOW, CRect(200, 200, 900, 900), NULL, NULL, 0, pContext); 
 	pFrameWnd->ShowWindow(SW_SHOW);
 	pFrameWnd->UpdateWindow();
 	this->m_pMainWnd = pFrameWnd;
